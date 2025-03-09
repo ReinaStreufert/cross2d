7df261382bd1627a7c3a8e69ae42c18868771df2
ReinaStreufert <49079389+ReinaStreufert@users.noreply.github.com>
2025-03-09 12:34:53 -0400

    Update ChildPlacementValidator.cs

--- UI/Layout/ChildPlacementValidator.cs
+++ UI/Layout/ChildPlacementValidator.cs
@@ -22,11 +22,14 @@ namespace Cross.UI.Layout
                 Node = node;
                 _TopLeft = new Point2DF();
                 _AbsoluteSize = new AbsoluteLayoutSize(new Size2DF(), new Padding2DF(), new Padding2DF());
+                _AttrContext = tree.AttributeProvider.CreateDependencyCollectorContext(node);
+                _AttrContext.OnDependencyMutated += DependencyMutationCallback;
             }
 
             private long _LastInvalidated;
             private Point2DF _TopLeft;
             private AbsoluteLayoutSize _AbsoluteSize;
+            private IDependencyCollectorAttrContext _AttrContext;
 
             public void SetInvalidated(DateTime t) => _LastInvalidated = t.Ticks;
 
@@ -39,8 +42,19 @@ namespace Cross.UI.Layout
                     var spatialContext = new SpatialContext<Size2DF>(_AbsoluteSize.PaddedSize);
                     foreach (var size in Node.ChildList.SelectMany(c => c.SizeValidator.Size))
                         spatialContext.Claim(size);
-
+                    _AttrContext.ReleaseDependencies();
+                    var organizerContext = new LayoutOrganizerContext(Node, _AttrContext, spatialContext);
+                    Node.Component.Organizer.OrganizeComponents(organizerContext);
+                    organizerContext.Apply(dirtyList);
                 }
+                foreach (var child in Node.ChildList)
+                    child.PlacementValidator.Validate(dirtyList);
+            }
+
+            private void DependencyMutationCallback()
+            {
+                SetInvalidated(DateTime.Now);
+                Tree._Destination.Invalidate();
             }
 
             private class LayoutOrganizerContext : ILayoutOrganizerContext
@@ -48,43 +62,30 @@ namespace Cross.UI.Layout
                 public Size2DF ClientSize => _Node.PlacementValidator.AbsoluteSize.PaddedSize;
                 public IEnumerable<ILayoutComponentOrganizer> Elements => _Organizers;
 
-                public LayoutOrganizerContext(LayoutNode node)
+                public LayoutOrganizerContext(LayoutNode node, IImmutableAttributeContext attrContext, SpatialContext<Size2DF> spatialContext)
                 {
                     _Node = node;
+                    _AttrContext = attrContext;
+                    _Organizers = node.ChildList
+                        .Select(n => new LayoutComponentOrganizer(n, spatialContext))
+                        .ToArray();
                 }
 
                 private LayoutNode _Node;
                 private LayoutComponentOrganizer[] _Organizers;
+                private IImmutableAttributeContext _AttrContext;
 
+                public T GetAttribute<T>(Key<T> key) => _AttrContext.GetAttribute(key);
+                public T GetAttribute<T>(IComponentTreeNode descendant, Key<T> key) => _AttrContext.GetAttribute(key);
+                public bool HasAttribute<T>(Key<T> key) => HasAttribute(key);
+                public bool HasAttribute<T>(IComponentTreeNode descendant, Key<T> key) => HasAttribute(descendant, key);
+                public bool TryGetAttribute<T>(Key<T> key, out T? val) => TryGetAttribute(key, out val);
+                public bool TryGetAttribute<T>(IComponentTreeNode descendant, Key<T> key, out T? val) => TryGetAttribute(descendant, key, out val);
 
-                public T GetAttribute<T>(Key<T> key)
+                public void Apply(DirtyRectList dirtyList)
                 {
-                    throw new NotImplementedException();
-                }
-
-                public T GetAttribute<T>(IComponentTreeNode descendant, Key<T> key)
-                {
-                    throw new NotImplementedException();
-                }
-
-                public bool HasAttribute<T>(Key<T> key)
-                {
-                    throw new NotImplementedException();
-                }
-
-                public bool HasAttribute<T>(IComponentTreeNode descendant, Key<T> key)
-                {
-                    throw new NotImplementedException();
-                }
-
-                public bool TryGetAttribute<T>(Key<T> key, out T? val)
-                {
-                    throw new NotImplementedException();
-                }
-
-                public bool TryGetAttribute<T>(IComponentTreeNode descendant, Key<T> key, out T? val)
-                {
-                    throw new NotImplementedException();
+                    foreach (var organizer in _Organizers)
+                        organizer.Apply(dirtyList);
                 }
             }
 
@@ -94,23 +95,23 @@ namespace Cross.UI.Layout
                 public LayoutSize SpatialSize => _Node.SizeValidator.Size;
                 public IComponentTreeNode ComponentNode => _Node;
 
-                public LayoutComponentOrganizer(LayoutNode node, SpatialContext<Size2DF> spatialContext, Rect2DF parentRect)
+                public LayoutComponentOrganizer(LayoutNode node, SpatialContext<Size2DF> spatialContext)
                 {
                     _Node = node;
                     Size = node.SizeValidator.Size.ToAbsolute(spatialContext.TotalSpace, spatialContext.RemainingSpace);
                     _OldTopLeft = _Node.PlacementValidator.TopLeft;
                     _TopLeft = new Point2DF();
-                    _ParentRect = parentRect;
+                    _ParentSize = spatialContext.TotalSpace;
                 }
 
                 private LayoutNode _Node;
                 private Point2DF _OldTopLeft;
                 private Point2DF _TopLeft;
-                private Rect2DF _ParentRect;
+                private Size2DF _ParentSize;
 
                 public void SetTopLeft(Point2DF topLeft)
                 {
-                    if (topLeft.X > _ParentRect.Width || topLeft.X < 0 || topLeft.Y > _ParentRect.Height || topLeft.Y < 0)
+                    if (topLeft.X > _ParentSize.Width || topLeft.X < 0 || topLeft.Y >= _ParentSize.Height || topLeft.Y < 0)
                         throw new ArgumentOutOfRangeException(nameof(topLeft));
                     _TopLeft = topLeft;
                 }
